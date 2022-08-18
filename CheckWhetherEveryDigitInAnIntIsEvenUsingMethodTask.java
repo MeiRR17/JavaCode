506e310aac7fd2a4928a5543efd76cec48b7c1eb
MeiRR17 <111447264+MeiRR17@users.noreply.github.com>
2022-08-18 11:56:35 +0300

    CheckWhetherEveryDigitInAnIntIsEvenUsingMethodTask

---
+++ CheckWhetherEveryDigitInAnIntIsEvenUsingMethodTask.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class CheckWhetherEveryDigitInAnIntIsEvenUsingMethodTask {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type your number: ");
+        int i = in.nextInt();
+        boolean bFinal = checkInt(i);
+        if (bFinal){
+            System.out.println("All  digits are even.");
+        }else{
+            System.out.println("There is one digits or more that is odd.");
+        }
+    }
+    public static boolean checkInt(int number){
+        int lastD = number % 10;
+
+        while (number != 0){
+            int lastDigit = number % 10;
+            number /= 10;
+
+            if(lastD != lastDigit){
+                return false;
+            }
+        }
+        return true;
+    }
+}
