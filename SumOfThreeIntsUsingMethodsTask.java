b4f8117720f6cc26cc2078dbf73cf469ddc574b9
MeiRR17 <111447264+MeiRR17@users.noreply.github.com>
2022-08-18 11:49:53 +0300

    SumOfThreeIntsUsingMethodsTask

---
+++ SumOfThreeIntsUsingMethodsTask.java
@@ -0,0 +1,10 @@
+public class SumOfThreeIntsUsingMethodsTask {
+
+    public static void main(String[] args) {
+        int s = sum(1, 2, 3);
+        System.out.println(s);
+    }
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
