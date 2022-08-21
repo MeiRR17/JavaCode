70fe6def1b6a57e9675e32204ef7427d56d30df5
MeiRR17 <111447264+MeiRR17@users.noreply.github.com>
2022-08-21 14:54:58 +0300

    PersonTask
    How do not get it... how do i set the balance?

--- Person.java
+++ Person.java
@@ -1,8 +1,9 @@
 public class Person {
-    static String name;
+    String name;
     int age;
     String city;
     String gender;
+    static double balance = 0;
     public static void main(String[] args) {
         Person obj1 = new Person();
         Person obj2 = new Person();
@@ -14,34 +15,48 @@ public class Person {
         obj2.city = "Pune";
         obj1.gender = "male";
         obj2.gender = "female";
-        System.out.println("obj1's name is: " + name + ".");
-        System.out.println("obj2's name is: " + name + ".");
+        System.out.println("obj1's name is: " + obj1.name + ".");
+        System.out.println("obj2's name is: " + obj2.name + ".");
         System.out.println("obj1's age is: " + obj1.age + ".");
         System.out.println("obj2's age is: " + obj2.age + ".");
         System.out.println("obj1's city is: " + obj1.city + ".");
         System.out.println("obj2's city is: " + obj2.city + ".");
         System.out.println("obj1's gender is: " + obj1.gender + ".");
         System.out.println("obj2's gender is: " + obj2.gender + ".");
-        eat();
-        eat();
-        study();
-        study();
-        sleep();
-        sleep();
-        play();
-        play();
+        System.out.println("current balance is: " + obj1.balance + ".");
+        obj1.eat();
+        obj2.eat();
+        obj1.study();
+        obj2.study();
+        obj1.sleep();
+        obj2.sleep();
+        obj1.play();
+        obj2.play();
+        obj1.addBalance(100);
+        obj2.addBalance(50);
+        obj1.addBalance(20);
+        //obj1.balance = ?;
 
     }
-    public static void eat(){
+
+
+
+    public void eat(){
         System.out.println(name + " is eating.");
     }
-    public static void study(){
+    public void study(){
         System.out.println(name + " is studying.");
     }
-    public static void sleep(){
+    public void sleep(){
         System.out.println(name + " is sleeping.");
     }
-    public static void play(){
+    public void play(){
         System.out.println(name + " is playing.");
+    }
+    public void addBalance (double balance){
+        System.out.println("current balance is: " + balance);
+    }
+    public double printBalance (double balance){
+        return balance;
     }
 }
