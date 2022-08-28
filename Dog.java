bc0694f56ca7dd95f22ef1e69e13fdc2d2ea86e3
MeiRR17 <m.b.z.dvir@gmail.com>
2022-08-28 19:36:34 +0300

    Made Dog class an interface which will print "waif waif" as and in the dogSound method and then called it in main method

--- Dog.java
+++ Dog.java
@@ -6,25 +6,25 @@ public class Dog implements JavaCode{
     private String color;
 
 
-    public void speak(){
-        System.out.println("waif waif dog is barking");
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
+//    public void speak(){
+//        System.out.println("waif waif dog is barking");
+//    }
+//
+//    public String getBreed() {
+//        return breed;
+//    }
+//
+//    public void setBreed(String breed) {
+//        this.breed = breed;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
 
     public void dogSound() {
         System.out.println("waif waif");
