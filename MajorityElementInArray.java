a7d57bd6eadd3029c98389dadeb53f85c22391a9
MeiRR17 <m.b.z.dvir@gmail.com>
2022-10-08 20:46:34 +0300

    .

--- MajorityElementInArray.java
+++ MajorityElementInArray.java
@@ -1,15 +1,18 @@
+import java.util.HashMap;
+
 public class MajorityElementInArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println();
-        int [] arr = new int[]{2, 2, 2, 0, 5, 5, 4, 3, 3};
+        int[] arr = new int[]{2, 2, 2, 0, 5, 5, 4, 3, 3};
         //if method turned nothing then there is no frequent else there is frequent
-        if(findFrequentNum(arr, arr.length) == 0){
+        if (findFrequentNum(arr, arr.length) == 0) {
             System.out.println("There is no frequent number in the current array.");
-        }else{
+        } else {
             System.out.println("The answer is: " + findFrequentNum(arr, arr.length) + ". Because this number is " + findCountOfFrequentNum(arr) + " times inside the array.");
         }
     }
-    public static int findFrequentNum(int[] arr, int i){
+
+    public static int findFrequentNum(int[] arr, int i) {
         int count;
         int max = 0;
         int currentMax = 0;
@@ -17,36 +20,34 @@ public class MajorityElementInArray {
         //Nested for loop
         for (int t = 0; t < i; t++){
             count = 0;
-            for (int j = 0; j < i; j++){
+            for (int j = 0; j < i; j++) {
                 //if they equal then count will increase by 1
-                if (arr[t] == arr[j]){
+                if (arr[t] == arr[j]) {
                     count++;
                 }
             }
             //if max is lower than count then max is now count
-            if (max < count){
+            if (max < count) {
                 max = count;
                 //saved the current max number in currentMax
                 currentMax = arr[t];
             }
         }
-        if(max == currentMax){
+        if (max == currentMax) {
             return 0;
         }
         //returned the final max number
         return currentMax;
     }
+
     public static int findCountOfFrequentNum(int [] arrI){
         //found count of the frequent number using nested for loop
         int count = 0;
         for (int i : arrI){
-            for (int j : arrI){
-                if (i == j){
+                if (i == findFrequentNum(arrI, arrI.length)){
                     count++;
                 }
             }
             return count;
-        }
-        return 0;
     }
 }
---
+++ MajorityElementInArrayUsingHashMap.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+
+public class MajorityElementInArrayUsingHashMap {
+    public static void main(String[] args){
+        int[] arr = new int[]{2, 2, 2, 0, 5, 5, 4, 3, 3};
+        HashMap<Integer, Integer> mapFinal = findFrequentNum(arr, arr.length);
+        for (int i : mapFinal.keySet()) {
+
+        }
+    }
+    public static HashMap<Integer, Integer> findFrequentNum(int [] arrI, int t) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arrI){
+            if (map.containsKey(i)){
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        for(int j : arrI){
+
+        }
+        }
+        return map;
+    }
+}
