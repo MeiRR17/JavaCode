5b3088b159d580999faefb77494090826337149b
MeiRR17 <m.b.z.dvir@gmail.com>
2022-10-12 00:07:10 +0300

    Find most frequent number using HashMap.

--- MajorityElementInArray.java
+++ MajorityElementInArray.java
@@ -1,13 +1,10 @@
-
 public class MajorityElementInArray {
     public static void main(String[] args) {
         int[] arr = new int[]{0, 5, 2, 5, 5, 5, 0, 3, 3};
         //if method turned nothing then there is no frequent else there is frequent
         int frequentNum = findFrequentNum(arr);
         System.out.println("The answer is: " + frequentNum + ". Because this number is " + findCountOfFrequentNum(arr, frequentNum) + " times inside the array.");
-
     }
-
     public static int findFrequentNum(int[] arr) {
         int count;
         int max = 0;
@@ -32,7 +29,6 @@ public class MajorityElementInArray {
         //returned the final max number
         return currentMax;
     }
-
     public static int findCountOfFrequentNum(int[] arrI, int ele) {
         //found count of the frequent number using nested for loop
         int count = 0;
--- MajorityElementInArrayUsingHashMap.java
+++ MajorityElementInArrayUsingHashMap.java
@@ -1,32 +1,32 @@
 import java.util.HashMap;
 
-
-public class MajorityElementInArrayUsingHashMap {
+public class MajorityElementInArrayUsingHashMap{
     public static void main(String[] args){
         int[] arr = new int[]{2, 2, 2, 0, 5, 5, 4, 3, 3};
-        HashMap<Integer, Integer> mapFinal = findFrequentNum(arr, arr.length);
-        for (int i : mapFinal.keySet()) {
-
-        }
+        int [] savedArr = findFrequentNumAndCount(arr);
+        System.out.println("The answer is: " + savedArr[0] + ". Because this number is " + savedArr[1] + " times inside the array.");
     }
-    public static HashMap<Integer, Integer> findFrequentNum(int [] arrI, int t) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int max = 0;
-        int current ;
+
+    public static int [] findFrequentNumAndCount(int [] arrI){
+
+        HashMap <Integer, Integer> map = new HashMap<>();
+        int maxElement = arrI[0];
+        int maxCount = 0;
+
         for (int i : arrI){
             if (map.containsKey(i)){
                 map.put(i, map.get(i) + 1);
             } else {
                 map.put(i, 1);
             }
-        for(int j : arrI){
-            if (max < map.get()){
-                current = map.get();
-                max = map.get();
-            }
         }
 
+        for(int key : arrI){
+            if (maxCount < map.get(key)){
+                maxElement = key;
+                maxCount = map.get(key);
+            }
         }
-        return map;
+        return new int[]{maxElement, maxCount};
     }
 }
