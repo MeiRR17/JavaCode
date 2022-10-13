f8d7665a8c22621da920be2576cb310b57b8cd22
MeiRR17 <m.b.z.dvir@gmail.com>
2022-10-13 13:20:29 +0300

    I think I'm done this one.

--- ThirdTask.java
+++ ThirdTask.java
@@ -1,17 +1,15 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ThirdTask {
     public static void main(String[] args){
-        int[] arr = new int[]{2, 2, 2, 0, 5, 5, 4, 3, 0};
-        int[] savedArr = findFrequentNumAndCount(arr);
-        System.out.println("Most frequent number is: " + savedArr[0] + ". Because this number is " + savedArr[1] + " times inside the array.");
-        System.out.println("Least frequent number is: " + savedArr[2] + ". Because this number is " + savedArr[3] + " times inside the array.");
+        int[] arr = new int[]{2, 0, 2, 0, 5, 5, 4, 3, 5};
+        findFrequentNumAndCount(arr);
     }
 
-    public static int[] findFrequentNumAndCount(int[] arrI){
-
+    public static void findFrequentNumAndCount(int[] arrI){
+        Arrays.sort(arrI);
         HashMap<Integer, Integer> map = new HashMap<>();
-        int maxElement = arrI[0];
         int maxCount = 0;
         //put the array inside the HashMap
         for (int i : arrI){
@@ -23,22 +21,37 @@ public class ThirdTask {
                 map.put(i, 1);
             }
         }
+
         //find the most frequent number
+
+        System.out.print("Most frequent number(s): ");
+
         for (int key : arrI){
             if (maxCount < map.get(key)){
-                maxElement = key;
                 maxCount = map.get(key);
             }
         }
-        int minElement = arrI[0];
-        int minCount = arrI.length;
+        for(int key : map.keySet()){
+            if(map.get(key) == maxCount){
+                System.out.print(key + ", ");
+            }
+        }
         //find the least frequent number
+
+        System.out.println();
+        System.out.print("Least frequent number(s): ");
+        int minCount = arrI.length;
         for (int key : map.keySet()){
             if (map.get(key) < minCount) {
                 minCount = map.get(key);
-                minElement = key;
             }
         }
-            return new int[]{maxElement, maxCount, minElement, minCount};
+
+        for(int key : map.keySet()){
+            if(map.get(key) == minCount){
+                System.out.print(key + ", ");
+            }
         }
+        System.out.println(".");
     }
+}
