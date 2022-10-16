18f98c9c5cc93087b2035454f1e7fd6befa5e308
MeiRR17 <m.b.z.dvir@gmail.com>
2022-10-16 11:25:38 +0300

    Alright so I did it as i thought it should be done but it doesnt work, it gives me an index error... what should I do?

--- FourthTask.java
+++ FourthTask.java
@@ -3,31 +3,27 @@ import java.util.HashMap;
 
 public class FourthTask {
     public static void main(String[] args) {
-        boolean result = checkIfValuesSame();
-        if (result) {
-            System.out.println("All the values in HashMap are same.");
-        } else {
-            System.out.println("Not all the values in HashMap are same.");
+        int firstValue = checkIfValuesSame();
+        HashMap<String, Integer> mainMap = new HashMap<>();
 
+        mainMap.put("A", 1);
+        mainMap.put("B", 1);
+        mainMap.put("C", 1);
+        mainMap.put("D", 1);
+        for (int value : mainMap.values()) {
+            if (value != firstValue) {
+                System.out.println("All the values in HashMap are same.");            }
         }
+        System.out.println("Not all the values in HashMap are same.");
+
     }
 
-    public static boolean checkIfValuesSame() {
+    public static int checkIfValuesSame() {
 
         HashMap<String, Integer> map = new HashMap<>();
 
-        map.put("A", 1);
-        map.put("B", 1);
-        map.put("C", 1);
-        map.put("D", 1);
 
-        int firstValue = new ArrayList<>(map.values()).get(0);
+        return new ArrayList<>(map.values()).get(0);
 
-        for (int value : map.values()) {
-            if (value != firstValue) {
-                return false;
-            }
-        }
-        return true;
     }
 }
--- SecondTask.java
+++ SecondTask.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 
 public class SecondTask{
     public static void main(String[] args) {
+
         System.out.println(combineHashMap());
     }
     public static HashMap<Character, Integer> combineHashMap() {
