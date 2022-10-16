1125d2c91c766c28b739d8b3fc22db5e3e232b11
MeiRR17 <m.b.z.dvir@gmail.com>
2022-10-16 17:18:00 +0300

    Check if all values in HashMap are equal to each other.

--- FourthTask.java
+++ FourthTask.java
@@ -3,27 +3,28 @@ import java.util.HashMap;
 
 public class FourthTask {
     public static void main(String[] args) {
-        int firstValue = checkIfValuesSame();
-        HashMap<String, Integer> mainMap = new HashMap<>();
-
-        mainMap.put("A", 1);
-        mainMap.put("B", 1);
-        mainMap.put("C", 1);
-        mainMap.put("D", 1);
-        for (int value : mainMap.values()) {
-            if (value != firstValue) {
-                System.out.println("All the values in HashMap are same.");            }
-        }
-        System.out.println("Not all the values in HashMap are same.");
-
-    }
-
-    public static int checkIfValuesSame() {
-
         HashMap<String, Integer> map = new HashMap<>();
 
+        map.put("A", 1);
+        map.put("B", 1);
+        map.put("C", 1);
+        map.put("D", 1);
+        if(checkIfValuesSame(map)){
+            System.out.println("All the values in HashMap are same.");
+        }else {
+            System.out.println("Not all the values in HashMap are same.");
+        }
+    }
 
-        return new ArrayList<>(map.values()).get(0);
+    public static boolean checkIfValuesSame(HashMap<String, Integer> map) {
 
+        int firstValue = new ArrayList<>(map.values()).get(0);
+
+        for (int value : map.values()) {
+            if (value != firstValue) {
+                return false;
+            }
+        }
+        return true;
     }
 }
