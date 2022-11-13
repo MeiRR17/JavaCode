563f113c0b42c6c00621037881255257a3d15b14
MeiRR <m.b.z.dvir@gmail.com>
2022-11-13 18:00:39 +0200

    current position

--- src/main.java
+++ src/main.java
@@ -9,20 +9,29 @@ public class main {
         System.out.println("Enter a char to reveal more the answer: ");
 
         ArrayList<Character> playerMove = new ArrayList<>();
-        String word = "Elephant";
+        String word = "Dog";
+        word = word.toLowerCase();
+
         int NumOfGuesses = 0;
         int NumOfGuessesLeft = 7;
 
-        word = word.toLowerCase();
-
-
-        while(true) {
-        String guess = in.nextLine();
-        playerMove.add(guess.charAt(0));
 
 
 
-        System.out.print(resultOfPlayerMove(playerMove, word));
+        while(true) {String guess = in.nextLine();playerMove.add(guess.charAt(0));
+//        int checking = 0;
+//        while(guess.length()>1) {
+//            System.out.println("Couldn't figure what you typed, please enter only one character.");
+//                checking = 1;
+//                break;
+//
+//        }
+//        if(checking==1){
+//            continue;
+//        }
+
+
+        resultOfPlayerMove(playerMove, word);
         System.out.println();
 
         if(winningPoint(playerMove, word)){
@@ -31,31 +40,38 @@ public class main {
         }
         if(!checkIfStringHasChar(playerMove, word)) {
             NumOfGuessesLeft--;
+
+            if (NumOfGuessesLeft > 2) {
+                System.out.println("Ohh, you missed that one please be calm, you have plenty of guesses left;)");
+            } else if (NumOfGuessesLeft > 1) {
+                System.out.println("Okay, seems like you could use a friend hand for this one!");
+            } else if (NumOfGuessesLeft > 0) {
+                System.out.println("Told you to use your friend...");
+            } else if (NumOfGuessesLeft == 0) {
+                System.out.println("Mhm, I think I had to bet for your lost.");
+            }
+        }else{
+            System.out.print("True! There is the character '");
+            showChar(playerMove, word);
+            System.out.println("' in the current word.");
+        }
+        NumOfGuesses++;
+        if (NumOfGuesses == 8) {
+            break;
         }
         System.out.println("You have " + NumOfGuessesLeft + " guesses left.");
-
-            NumOfGuesses++;
-        for(int index = 0;index<word.length();index++) {
-            if (NumOfGuesses == 7) {
-                if (!checkIfStringHasChar(playerMove, word)) {
-                    break;
-                }
-            }
-        }
 
         }
         System.out.println("YOU LOST THE GAME :/");
     }
-    static String resultOfPlayerMove(ArrayList <Character> playerMove, String currentWord) {
-        String underscore = "_";
+    static void resultOfPlayerMove(ArrayList <Character> playerMove, String currentWord) {
         for (int index = 0; index < currentWord.length(); index++) {
             if (playerMove.contains(currentWord.charAt(index))) {
                 System.out.print(currentWord.charAt(index));
             }else{
-                System.out.print(underscore);
+                System.out.print("_");
             }
         }
-        return underscore;
     }
     static boolean checkIfStringHasChar(ArrayList <Character> playerMove, String currentWord) {
         for (int index = 0; index < currentWord.length(); index++) {
@@ -74,5 +90,11 @@ public class main {
         }
         return (winningCount == currentWord.length());
     }
+    static void showChar(ArrayList <Character> playerMove, String currentWord) {
+        for (int i = 0; i < currentWord.length(); i++) {
+            if (playerMove.contains(currentWord.charAt(i))) {
+                System.out.print(currentWord.charAt(i));            }
+        }
 
+    }
 }
