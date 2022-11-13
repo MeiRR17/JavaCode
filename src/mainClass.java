c9a17e3d740d40be20ae04705a4a324f5b517447
MeiRR <m.b.z.dvir@gmail.com>
2022-11-14 00:20:42 +0200

    UPDATE 0020 I THINK I FINISH BETA VERSION.

--- src/mainClass.java
+++ src/mainClass.java
@@ -6,66 +6,76 @@ import java.util.Scanner;
 public class mainClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter a char to reveal more the answer: ");
-
         ArrayList<Character> playerMove = new ArrayList<>();
-        String word = "Dog";
-        word = word.toLowerCase();
-
-        int NumOfGuesses = 0;
+        String word = "dog";
         int NumOfGuessesLeft = 7;
+        System.out.println("""
+                Playing Hangman:
+                                
+                Hangman is an old school favorite, a word game where the goal is simply to find the missing word or words.
 
+                You will be presented with a number of blank spaces representing the missing letters you need to find.
 
+                Use the keyboard to guess a letter (I recommend starting with vowels).
 
+                If your chosen letter exists in the answer, then all places in the answer where that letter appear will be revealed.
 
-        while(true) {String guess = in.nextLine();playerMove.add(guess.charAt(0));
-        int checking = 0;
-        while(guess.length()>1) {
-            System.out.println("Couldn't figure what you typed, please enter only one character.");
-                checking = 1;
-                break;
+                After you've revealed several letters, you may be able to guess what the answer is and fill in the remaining letters.
 
-        }
-        if(checking==1){
-            continue;
-        }
+                Be warned, every time you guess a letter wrong you loose a life out of 7.
+                
+                Solve the puzzle before the hangman dies!
+                                
+                PRESS Enter FOR PLAYING...""");
 
+        String start = in.nextLine();
+        System.out.println("Type a char to reveal more the answer:");
         resultOfPlayerMove(playerMove, word);
         System.out.println();
+        while(true) {
+            String guess = in.nextLine();
+            playerMove.add(guess.charAt(0));
+            int checking = 0;
 
-        if(winningPoint(playerMove, word)){
-            System.out.println("Great! You won the game!");
-            break;
-        }
+            while(guess.length()>1) {
+                System.out.println("Couldn't figure what you typed, please enter only one character.");
+                checking = 1;
+                break;
+            }
+            if(checking==1){
+                continue;
+            }
 
-        /////////////////////////////////////////////////////////////////////////////////////////////////
-        if(!checkIfStringHasChar(playerMove, word)) {
-            NumOfGuessesLeft--;
+            resultOfPlayerMove(playerMove, word);
+            System.out.println();
+
+            if(winningPoint(playerMove, word)){
+                System.out.println("Wow! You won the game!");
+                break;
+            }
+
+            if(!word.contains(guess)) {
+                NumOfGuessesLeft--;
 
             if (NumOfGuessesLeft > 2) {
-                System.out.println("Ohh, you missed that one please be calm, you have plenty of guesses left;)");
+                System.out.println("Ohh, you missed that one, don't worry about it so much, you still have plenty of guesses left;)");
             } else if (NumOfGuessesLeft > 1) {
                 System.out.println("Okay, seems like you could use a friend hand for this one!");
             } else if (NumOfGuessesLeft > 0) {
                 System.out.println("Told you to use your friend...");
             } else if (NumOfGuessesLeft == 0) {
-                System.out.println("Mhm, I think I had to bet for your lost.");
+                System.out.println("Mhm, I think I had to bet for your losing.");
             }
-        }else{
-            System.out.print("True! There is the character '");
-            showChar(playerMove, word);
-            System.out.println("' in the current word.");
-        }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        NumOfGuesses++;
-        if (NumOfGuesses == 8) {
-            break;
-        }
-        System.out.println("You have " + NumOfGuessesLeft + " guesses left.");
-
-        }
-        if(!winningPoint(playerMove, word)) {
-            System.out.println("YOU LOST THE GAME :/");
+            }else{
+                System.out.print("True! There is the character '");
+                showChar(playerMove, word);
+                System.out.println("' in the current word.");
+            }
+            if (NumOfGuessesLeft == -1) {
+                System.out.println("YOU LOST THE GAME :/");
+                break;
+            }
+            System.out.println("You have " + NumOfGuessesLeft + " guesses left.");
         }
     }
     static void resultOfPlayerMove(ArrayList <Character> playerMove, String currentWord) {
@@ -76,14 +86,6 @@ public class mainClass {
                 System.out.print("_");
             }
         }
-    }
-    static boolean checkIfStringHasChar(ArrayList <Character> playerMove, String currentWord) {
-        for(int i = 0;i<currentWord.length();i++){
-            if(playerMove.contains(currentWord.charAt(i))){
-                return true;
-            }
-        }
-        return false;
     }
     static boolean winningPoint(ArrayList <Character> playerMove, String currentWord) {
         int winningCount = 0;
@@ -97,8 +99,8 @@ public class mainClass {
     static void showChar(ArrayList <Character> playerMove, String currentWord) {
         for (int i = 0; i < currentWord.length(); i++) {
             if (playerMove.contains(currentWord.charAt(i))) {
-                System.out.print(currentWord.charAt(i));            }
+                System.out.print(currentWord.charAt(i));
+            }
         }
-
     }
 }
