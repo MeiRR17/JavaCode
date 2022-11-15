236e8711d96e9fd6df1e01ac350f1b12cd52670a
MeiRR <m.b.z.dvir@gmail.com>
2022-11-15 13:37:18 +0200

    1337 1511

--- src/practiceFileHandling.java
+++ src/practiceFileHandling.java
@@ -1,32 +1,42 @@
 package src;
 
 import java.io.*;
-import java.util.Arrays;
 
 public class practiceFileHandling {
     public static void main(String[] args) {
-        int [] names = new int[10];
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
             writer.write("This is a file");
             writer.write("\nHere is another line in the file.");
-            writer.close();
-            for(int i = 0;i< names.length;i++){
-                names[i] += i;
-                writer.write("\n" + i);
-                System.out.println(Arrays.toString(names));
+            File myFile0 = new File("fileName.txt");
+            if(myFile0.createNewFile()){
+                System.out.println(myFile0 + "was created.");
             }
+            File myFile = new File("C:\\Users\\MyName\\filename.txt");
+
+            if(myFile.exists()){
+                System.out.println("Absolute path: " + myFile.getAbsolutePath());
+                System.out.println("File name: " + myFile.getName());
+                System.out.println("Absolute path: " + myFile.getAbsolutePath());
+                System.out.println("Writeable: " + myFile.canWrite());
+                System.out.println("Readable " + myFile.canRead());
+                System.out.println("File size in bytes " + myFile.length());
+            }else{
+                System.out.println("Current file doesn't exist.");
+            }
+
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
-            String line;
-            while((line = reader.readLine()) != null){
-                System.out.println(line);
-            }
-                System.out.println(reader.readLine());
-            reader.close();
+            BufferedReader reader1 = new BufferedReader(new FileReader("output.txt"));
+            BufferedReader reader0 = new BufferedReader(new FileReader("fileName.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("fileName.txt"));
+
+            System.out.println(reader1.readLine());
+            System.out.println(reader0.readLine());
+            System.out.println(reader.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
