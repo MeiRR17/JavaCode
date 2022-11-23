40c05f85988f7d39c63c51b5696c857e26e98b82
MeiRR <m.b.z.dvir@gmail.com>
2022-11-23 16:06:31 +0200

    Added some things 1606 update

--- src/librarySystem/Books.java
+++ src/librarySystem/Books.java
@@ -1,5 +1,47 @@
 package src.librarySystem;
 
 public class Books extends Library{
+    //book should have title author year number of pages
+    private String title;
+    private String author;
+    private int yearOfRelease;
+    private int numberOfPages;
+    public Books(String booksTitle, String booksAuthor, int booksYearOfRelease, int booksNumberOfPages){
+        title = booksTitle;
+        author = booksAuthor;
+        yearOfRelease = booksYearOfRelease;
+        numberOfPages = booksNumberOfPages;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
 }
--- src/librarySystem/Library.java
+++ src/librarySystem/Library.java
@@ -5,18 +5,64 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
+    static void timeDelay() {
+        try {
+            Thread.sleep(800);
+        }
+        catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    public static int timeInDay() {
+        LocalTime time = LocalTime.now();
+        return time.getHour();
+    }
+    public static void signInGreeting(String currentUserName){
+        if(timeInDay()>=12&&timeInDay()<17){
+            System.out.println("Good afternoon " + currentUserName + ".");
+        }else if(timeInDay()>=17&&timeInDay()<21){
+            System.out.println("Good evening " + currentUserName + ".");
+        }else if(timeInDay()>=21&&timeInDay()<5){
+            System.out.println("Good night " + currentUserName + ".");
+        }else{
+            System.out.println("Good morning " + currentUserName + ".");
+        }
+    }
+
+
+
+
+
     public static void main(String[] args) {
         ArrayList<User> registered = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         String currentUserName = "";
         System.out.println("welcome to the library.");
+        System.out.println("Select the account type you would like to use");
+        System.out.println("(User/Admin)");
+
+
+
+
+        boolean select = false;
         int counter = 3;
+
+        String selectUser = in.nextLine();
         while (true) {
-            System.out.println("Sign in for current users or register.");
 
+            while(!selectUser.equals("user")) {
+                if (selectUser.equals("admin")) {
+                    select = true;
+                    break;
+                }
+            else {
+                    System.out.println("Couldn't understand that...");
+                }
+            }
+            System.out.println("sign in/register");
             String signInOrRegister = in.nextLine().toLowerCase();
-
             if (signInOrRegister.equals("sign in")) {
+
                 System.out.println("Enter your account username:");
                 String signUserName = in.nextLine();
                 System.out.println("Enter your password: ");
@@ -55,23 +101,26 @@ public class Library {
                 System.out.println("Okay, your account has been created.");
 
             } else {
-                System.out.println("Could not understand that...");
+                System.out.println("Could not understand what you typed...");
+                timeDelay();
             }
         }
-        if(timeInDay()>=12&&timeInDay()<17){
-            System.out.println("Good afternoon " + currentUserName + ".");
-        }else if(timeInDay()>=17&&timeInDay()<21){
-            System.out.println("Good evening " + currentUserName + ".");
-        }else if(timeInDay()>=21&&timeInDay()<5){
-            System.out.println("Good night " + currentUserName + ".");
+
+        //admin  set
+        if(select){
+            System.out.println("You logged in successfully to an admin account!");
+            signInGreeting(currentUserName);
+            System.out.println("Do you want to add new books or remove particular books?");
         }else{
-            System.out.println("Good morning " + currentUserName + ".");
+            System.out.println("You logged in successfully to an user account!");
+            signInGreeting(currentUserName);
+            System.out.println("Do you want to borrow a book or restore a book?");
         }
-        System.out.println("Welcome to Library System.");
+
+
+
+
 
     }
-    public static int timeInDay() {
-        LocalTime time = LocalTime.now();
-        return time.getHour();
-    }
+
 }
