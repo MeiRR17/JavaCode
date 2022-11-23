d1e3696fb237450f4fc95d97e1ba7a529a0585dc
MeiRR <m.b.z.dvir@gmail.com>
2022-11-23 12:35:56 +0200

    Added some things 1235 update

--- src/librarySystem/Library.java
+++ src/librarySystem/Library.java
@@ -1,34 +1,40 @@
 package src.librarySystem;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
     public static void main(String[] args) {
         ArrayList<User> registered = new ArrayList<>();
-
         Scanner in = new Scanner(System.in);
-
+        String currentUserName = "";
         System.out.println("welcome to the library.");
         int counter = 3;
         while (true) {
             System.out.println("Sign in for current users or register.");
 
             String signInOrRegister = in.nextLine().toLowerCase();
-            System.out.println(signInOrRegister);
+
             if (signInOrRegister.equals("sign in")) {
                 System.out.println("Enter your account username:");
                 String signUserName = in.nextLine();
                 System.out.println("Enter your password: ");
                 String signPassword = in.nextLine();
 
-
+                boolean breakLoop = true;
                 for (User user : registered) {
                     if (signUserName.equals(user.getUsername())&&signPassword.equals(user.getPassword())) {
+                        breakLoop = false;
+                        currentUserName = user.getFullName();
                         break;
                     } else {
                         System.out.println("Your password, username or fullName is not on the system, please try again.");
                     }
+                }
+                //way out of loop if username & password was correct
+                if(!breakLoop){
+                    break;
                 }
                 counter--;
 
@@ -52,6 +58,20 @@ public class Library {
                 System.out.println("Could not understand that...");
             }
         }
+        if(timeInDay()>=12&&timeInDay()<17){
+            System.out.println("Good afternoon " + currentUserName + ".");
+        }else if(timeInDay()>=17&&timeInDay()<21){
+            System.out.println("Good evening " + currentUserName + ".");
+        }else if(timeInDay()>=21&&timeInDay()<5){
+            System.out.println("Good night " + currentUserName + ".");
+        }else{
+            System.out.println("Good morning " + currentUserName + ".");
+        }
+        System.out.println("Welcome to Library System.");
 
+    }
+    public static int timeInDay() {
+        LocalTime time = LocalTime.now();
+        return time.getHour();
     }
 }
